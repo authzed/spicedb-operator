47bf3aa7e6ed0825d3ac6839758aba9cf7dd8c40
Evan Cordell <cordell.evan@gmail.com>
2022-05-06 08:55:24 -0400

    add warnings when config has non-blocking problems

--- pkg/controller/handlers/config_change.go
+++ pkg/controller/handlers/config_change.go
@@ -3,6 +3,8 @@ package handlers
 import (
 	"context"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
 	"github.com/authzed/spicedb-operator/pkg/apis/authzed/v1alpha1"
 	"github.com/authzed/spicedb-operator/pkg/controller/handlercontext"
 	"github.com/authzed/spicedb-operator/pkg/libctrl"
@@ -27,15 +29,24 @@ func NewConfigChangedHandler(ctrls libctrl.HandlerControls, cluster *v1alpha1.Sp
 
 func (c *ConfigChangedHandler) Handle(ctx context.Context) {
 	secretHash := handlercontext.CtxSecretHash.Value(ctx)
-	if c.cluster.GetGeneration() != c.cluster.Status.ObservedGeneration || secretHash != c.cluster.Status.SecretHash {
-		c.cluster.Status.ObservedGeneration = c.cluster.GetGeneration()
-		c.cluster.Status.SecretHash = secretHash
-		c.SetStatusCondition(v1alpha1.NewValidatingConfigCondition(secretHash))
-		if err := c.patchStatus(ctx, c.cluster); err != nil {
+	status := &v1alpha1.SpiceDBCluster{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       v1alpha1.SpiceDBClusterKind,
+			APIVersion: v1alpha1.SchemeGroupVersion.String(),
+		},
+		ObjectMeta: metav1.ObjectMeta{Namespace: c.cluster.Namespace, Name: c.cluster.Name, Generation: c.cluster.Generation},
+		Status:     *c.cluster.Status.DeepCopy(),
+	}
+
+	if c.cluster.GetGeneration() != status.Status.ObservedGeneration || secretHash != status.Status.SecretHash {
+		status.Status.ObservedGeneration = c.cluster.GetGeneration()
+		status.Status.SecretHash = secretHash
+		status.SetStatusCondition(v1alpha1.NewValidatingConfigCondition(secretHash))
+		if err := c.patchStatus(ctx, status); err != nil {
 			c.RequeueErr(err)
 			return
 		}
 	}
-	ctx = handlercontext.CtxClusterStatus.WithValue(ctx, c.cluster)
+	ctx = handlercontext.CtxClusterStatus.WithValue(ctx, status)
 	c.next.Handle(ctx)
 }
